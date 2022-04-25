be089582ec7ee8df6f21b1ac72a454c8853faaee
karenlyyys <99241654+karenlyyys@users.noreply.github.com>
2022-04-25 12:42:23 -0700

    changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-
+const path = require("path")
 const app = express();
 const PORT = 8080;
 
@@ -7,8 +7,8 @@ app.get("/", (req, res)=>{
     res.send("welcome to my flix")
 })
 
-app.get("/register", (req, res)=>{
-    res.send("register")
+app.get("/documentation", (req, res)=>{
+    res.sendFile(path.join(__dirname,'/public/documentation.html'));
 })
 
 app.listen(PORT, ()=>console.log("App is running"))
